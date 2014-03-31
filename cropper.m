39ed37301ab206a4d428d4590ec74122149e61b6
sisi <sisichen@berkeley.edu>
2014-03-31 13:30:23 -0700

    use matlab native image handling instead of dipimage arrays

--- cropper.m
+++ cropper.m
@@ -1,10 +1,14 @@
 
-function [midX midY] = cropper()
+function [midX midY] = cropper(varargin)
 
-% This function takes in a montaged image and crops all of the patterned
-% islands into an aligned stack of squares of size sqLength. The original 
-% xpitch and ypitch that had been calibrated for sl19_w1_d0 is 770.5, and
-% 774.1. 
+% The function then prompts the user for a montaged image and crops all of 
+% the patterned islands into an aligned stack of squares of size sqLength. 
+% The original xpitch and ypitch that had been calibrated for sl19_w1_d0 is 
+% 770.5, and 774.1. 
+%
+% The first argument is the x-coordinate of the centroid of the first
+% square. The second argument is the y-coordinate of the centroid of the
+% first square.
 % 
 % For day 0 images and day 6 stained images, make sure to merge and
 % properly adjust the scaling of multichannel images before cropping. The
@@ -16,8 +20,8 @@ sqLength = 300;
 numRows = 19;
 numCols = 13;
 numSq = numRows*numCols;
-xpitch = 770.5;
-ypitch = 774.1;
+xpitch = 774.1; %switch for dipimage arrays
+ypitch = 770.5;
 
 % Ask user to select the file: 
 
@@ -27,24 +31,18 @@ if isequal(filename,0) || isequal(pathname,0)
        return;
     else
        disp(['User selected ', fullfile(pathname, filename)])
-       montIm = readim(strcat(pathname,filename), '');
+       montIm = imread([pathname,filename], 'tif');
 end
 
 % Determine the coordinates of the middle of the first square by: 
 % 1. Asking the user to enter it manually. 
 % 2. Crop an upper region for identification of first square
 
-a = input('Can you input first square coordinates (Y/N)? ', 's');
+a = nargin;
 
-if (a == 'y')||(a == 'Y')
-    
-    midX=input('Please enter the x coordinate of the first square in pixels: ');
-    midY=input('Please enter the y coordinate of the first square in pixels: ');
-
-else
+if nargin<2 %if insufficient number of arguments
     
     smallMontIm = montIm(1:3000,1:1000);
-    smallMontIm
     display('Pick corners in a clockwise direction starting from top left');
     
     b='N';
@@ -56,15 +54,24 @@ else
     midX = round(mean(x));
     midY = round(mean(y));
     
+else 
+    
+    midX=round(varargin{1});
+    midY=round(varargin{2});
+    
+    if ~isnumeric(midX)&&isnumeric(midY)
+        error('Please input numerical coordinates');
+    end
+   
 end
 
 
 % Populate list of x and y for centroids coordinates: 
 
-allMidX = zeros(1, numCols); %all column coordinates
-allMidY = zeros(1, numRows); %all row coordinates
+allMidX = zeros(1, numRows); %all column coordinates
+allMidY = zeros(1, numCols); %all row coordinates
 
-for i=1:numCols
+for i=1:numRows
     if i==1
         allMidX(i)=midX;
     else
@@ -72,7 +79,7 @@ for i=1:numCols
     end
 end
 
-for j=1:numRows
+for j=1:numCols
     if j==1
         allMidY(j)=midY;
     else
@@ -85,10 +92,10 @@ allMidY=round(allMidY);
 
 % Make squares at each center coordinate and append to the image array
 
-imstack = newimar(numSq);
-for j = 1:numRows
-    for i = 1:numCols
-        currSq = makesquare(allMidX(i), allMidY(j), sqLength, montIm);
+imstack = cell(numSq,1);
+for i = 1:numRows %need to switch if using dipImage
+    for j = 1:numCols
+        currSq = makesquare(allMidX(i), allMidY(j), sqLength, montIm);        
         imstack{i+numCols*(j-1)} = currSq;
     end
 end
--- makesquare.m
+++ makesquare.m
@@ -4,8 +4,8 @@ function sqIm=makesquare(midX,midY, sqLength, montIm)
 % square coordinates exceed the size of montIm, then a blank square is
 % returned. 
 
-xlimit = size(montIm, 1);
-ylimit = size(montIm, 2);
+ylimit = size(montIm, 1); %need to switch if using dipimage
+xlimit = size(montIm, 2);
 
 halfSqLength=sqLength/2;
 
@@ -14,8 +14,8 @@ rightX = midX + halfSqLength - 1; % -1 to keep the size at (sqLength,sqLength)
 upY = midY - halfSqLength;
 downY = midY + halfSqLength - 1; % - 1 to keep the size at (sqLength,sqLength)
 
-if (rightX>xlimit)|(downY>ylimit)
-    sqIm=dip_image(zeros(sqLength,sqLength));
+if (rightX>xlimit)||(downY>ylimit)
+    sqIm=zeros(sqLength,sqLength);
 else
-    sqIm = montIm(leftX:rightX, upY:downY);
+    sqIm = montIm(upY:downY,leftX:rightX); %need to change orientation if using dipimage
 end
