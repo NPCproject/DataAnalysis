41678389becfb78709e85abd9e4472d2ab03eef8
sisi <sisichen@berkeley.edu>
2014-09-09 16:11:11 -0700

    edit help text for batchCrop

--- batchCrop.m
+++ batchCrop.m
@@ -5,11 +5,11 @@ function batchCrop(slideNum, coordinates)
 % Take in the pathname of the image montages and the coordinates according
 % to the well number. Using these data, batchCrop creates cropped stacks in
 % the same folder for each well. 
-
+%
 % slideNum (number): number of the slide
 % coordinates (cell array): [w1] [w2] [w3] [w4], each cell of array is a
 % two number vector giving the x1 and y1 centroid coordinates of the upper
-% leftmost pattern. 
+% leftmost pattern. e.g. coords={[153,133],[374,209],[599,224],[821,274]}
 
 pathname = ['/Users/sisichen/Dropbox/Dropbox Data Stacks/slide ' num2str(slideNum) ' stacks/'];
 filenames = uigetfile([pathname '.tif'], 'MultiSelect', 'on');
